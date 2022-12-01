8fe511727ac57ecaf6241f21f7ccebda2808493f
antoninobajeli <antoninobajeli@gmail.com>
2022-12-01 13:15:09 +0100

    cartellazione quisi_desk

---
+++ quisi_desk/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts('https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.1/firebase-messaging.js');
+firebase.initializeApp({
+    apiKey: 'AIzaSyBbF6KdB2jxIdibhUybICMHD2opLTriJYs',
+            appId: '1:545603182859:web:58dfa52f696487e076420e',
+            messagingSenderId: '545603182859',
+            projectId: 'quisi-fire',
+            authDomain: 'quisi-fire.firebaseapp.com',
+            storageBucket: 'quisi-fire.appspot.com',
+            measurementId: 'G-MM88E1Y2HX'
+});
+const messaging = firebase.messaging();
