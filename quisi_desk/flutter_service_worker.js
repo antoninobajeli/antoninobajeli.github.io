fb86f0480fe878636cd65fb34559a823a9ec567b
antoninobajeli <antoninobajeli@gmail.com>
2023-01-27 17:58:14 +0100

    BUMP for 0.0.69+69

--- quisi_desk/flutter_service_worker.js
+++ quisi_desk/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "c9096a135ab25324b0f059af8fa1c7d4",
-"index.html": "3fb4721c3e759d1dd4e3a655cefdd531",
-"/": "3fb4721c3e759d1dd4e3a655cefdd531",
+  "version.json": "3b7175e0b0cac1d19370b4162365dafd",
+"index.html": "cd7e1d90ca4bfbc2444998006619be4d",
+"/": "cd7e1d90ca4bfbc2444998006619be4d",
 "firebase-messaging-sw.js": "5c5b7cb117d5caf4b5f7699dfb37a6ce",
-"main.dart.js": "a4a5cbdadad702174ea92e81d54ef3c9",
+"main.dart.js": "fce544581a6c25b6d7ef0a75687e0782",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5f9441042e15e154d6e47b81e6460e1d",
 "icons/Icon-192.png": "88033c8efdd27971e47f5123f16f4175",
